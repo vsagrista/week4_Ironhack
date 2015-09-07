c01e69f936c86c8add4d918207354a88bee61ebf
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-07 19:34:35 +0200

    Third iteration, display current contact by url Id through new method in controller

--- addressbook/app/controllers/contacts_controller.rb
+++ addressbook/app/controllers/contacts_controller.rb
@@ -2,4 +2,8 @@ class ContactsController < ApplicationController
   def index
   	@contacts = Contact.all.sort{|a,b| a <=> b}
   end
+  def show
+		id = params[:id]
+		@current_user = Contact.find(id)
+  end  
 end
--- addressbook/config/routes.rb
+++ addressbook/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/" => "contacts#index"
+  get "/contacts/:id" => "contacts#show" 
 end
