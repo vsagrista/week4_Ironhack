9b6ca636df2c7983620a2b2d39b2f5e004a820e8
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-07 19:00:31 +0200

    Executed seed migration to store default contacts. Used gem faker to assign random names

--- addressbook/db/seeds.rb
+++ addressbook/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+	Contact.create(name: Faker::Name.name, address: Faker::Address.street_address, email: Faker::Internet.email, phone: Faker::PhoneNumber.cell_phone)
+end
