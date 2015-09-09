229d74e0f24fa5a82ed28bfe92d6a7c79b6ca20b
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-09 18:07:47 +0200

    Created Products controller and Product model, ran migration. Basic architecture of app built

--- ebaysite/db/schema.rb
+++ ebaysite/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909154303) do
+ActiveRecord::Schema.define(version: 20150909155614) do
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "deadline"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
