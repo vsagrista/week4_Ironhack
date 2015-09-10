c9162bad94d588be70220912709c94db53e28315
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-10 18:48:37 +0200

    Added validations to Product model

--- ebaysite/app/models/product.rb
+++ ebaysite/app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
+	
+	validates :title, :description,:deadline, presence: true
+	validates :description, length: { maximum: 2000 }
+	validates :title, length: { maximum: 100 }
+	validates :title,:description, uniqueness: true
+
 	belongs_to :user
 	has_many :bids
 end
+
