59f256c7cbcced098eeeead6c5e15a8f92a78c5d
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-11 22:49:23 +0200

    Made validation methods on the Bid model

--- ebaysite/app/controllers/bids_controller.rb
+++ ebaysite/app/controllers/bids_controller.rb
@@ -6,19 +6,21 @@ class BidsController < ApplicationController
   def create
     user = User.where(email: params[:bid][:user])
     amount = params[:bid][:amount]
+
     @bid = Bid.create(amount: amount, user_id: user[0].id, product_id: params[:product_id].to_i )
     @product = Product.find(@bid.product_id)
-    min_bid = @product.min_bid
-    winner = @product.bids.order("amount DESC").first
-
-    if Time.now >= @product.deadline && amount.to_i > min_bid
-      @winner_amount = winner.amount
-      @winner_user = winner.user.name
-      render :show
+    if @bid.errors.blank? == false
+      render  :error
     else
-      @top_bid = winner.amount
-      @top_user = winner.user.name
-      render :show
+      if Time.now >= @product.deadline
+        @winner_amount = amount
+        @winner_user = user[0].name
+        render :show
+      else
+        @winner_bid = @product.bids.order("amount DESC").first
+        @top_user = @winner_bid.user.name
+        render :show
+      end
     end
   end
 end
--- ebaysite/app/models/bid.rb
+++ ebaysite/app/models/bid.rb
@@ -1,4 +1,42 @@
 class Bid < ActiveRecord::Base
-	belongs_to :product
-	belongs_to :user
+  belongs_to :product
+  belongs_to :user
+
+  validate :has_errors?
+  def has_errors?
+    @product = Product.find(self.product_id)
+    @min_bid= @product.min_bid
+    if @product.bids.all.length == 0
+      is_first_bid?
+    else
+      @winner_bid = @product.bids.order("amount DESC").first
+      @winner_amount = @winner_bid.amount
+      @min_bid= @product.min_bid
+      if reached_deadline?(@product)
+      else is_bigger_bid?(@min_bid,@winner_amount)
+      end
+    end
+  end
+
+  def is_first_bid?
+    if amount.to_i < @min_bid
+      errors.add(:field, "Too small!")
+      @error = "Number is too small!"
+
+    end
+  end
+
+  def reached_deadline?(product)
+    if Time.now >= product.deadline
+      errors.add(:field, "Deadline not yet.")
+
+    end
+  end
+
+  def is_bigger_bid?(min_bid,winner_amount)
+    if amount.to_i < min_bid || amount.to_i < winner_amount || amount.to_i == winner_amount
+      errors.add(:field, "too small ")
+
+    end
+  end
 end
