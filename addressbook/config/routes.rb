5df6b221d12ae24afbd6d34a3ef75d0b9e1ba536
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-01 18:32:44 +0100

    Improved views and fixed routes

--- addressbook/config/routes.rb
+++ addressbook/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "contacts#index"
+  root to: "contacts#index"
   get "/contacts/new" => "contacts#new"
   get "/contacts/:id" => "contacts#show", as: :contact
   post "/contacts" => "contacts#create"
-  patch "/contacts/:id" => "contacts#favorite"
+  get "/contacts/favorites" => "contacts#favorite"
   post "/contacts/find_contact" => "contacts#find_contacts"
 end
